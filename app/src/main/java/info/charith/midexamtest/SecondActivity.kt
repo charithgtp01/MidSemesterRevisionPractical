07dd5b91b50e928dc74f3ff38c9223d430557b1e
Charith <vinodyav@tellko.com>
2023-01-21 17:16:34 +0530

    Files added

---
+++ app/src/main/java/info/charith/midexamtest/SecondActivity.kt
@@ -0,0 +1,22 @@
+package info.charith.midexamtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import info.charith.midexamtest.databinding.ActivityMainBinding
+import info.charith.midexamtest.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val dataBinding: ActivitySecondBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_second)
+
+        dataBinding.name = intent.getStringExtra(getString(R.string.name))!!
+        dataBinding.email = intent.getStringExtra(getString(R.string.email))!!
+
+//        dataBinding.tvResult.text = "Hi $name,\n Your email: $email is registered successfully"
+    }
+}
---
+++ appsidemenu/src/main/java/info/charith/appsidemenu/SlideshowFragment.kt
@@ -0,0 +1,26 @@
+package info.charith.appsidemenu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import info.charith.appsidemenu.databinding.FragmentSlideshowBinding
+
+class SlideshowFragment : Fragment() {
+
+    private lateinit var binding: FragmentSlideshowBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_slideshow,container, false)
+
+        return binding.root
+    }
+
+}
